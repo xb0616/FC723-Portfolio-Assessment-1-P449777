428ada1d5a5aebebbdbafcc1ba3b0a4c146de746
xb0616 <2780318633@qq.com>
2025-01-21 08:48:21 +0000

    Update FC723 Portfolio Assessment 1 - Application.py
    turn GCD() to a function

--- FC723 Portfolio Assessment 1 - Application.py
+++ FC723 Portfolio Assessment 1 - Application.py
@@ -1,11 +1,15 @@
 a=int(input("Please enter the first number A"))
 b=int(input("Please enter the second number B"))
 
-while a*b!=0:
-    if a>b :
-        r=a%b
-        a,b=b,r
-    else:
-        a,b=b,a
+def Euclidean(a,b):
+    while a*b!=0:
+        if a>b :
+            r=a%b
+            a,b=b,r
+        else:
+            a,b=b,a
+    return a
+
+a = Euclidean(a,b)
         
 print(f"The GCD of A and B is {a}")
