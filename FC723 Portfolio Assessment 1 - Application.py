c5af9e83847cb3b59e22c7bc6dbf5ee3e67d444b
xb0616 <2780318633@qq.com>
2025-01-21 09:40:27 +0000

    Update FC723 Portfolio Assessment 1 - Application.py
    final code part

--- FC723 Portfolio Assessment 1 - Application.py
+++ FC723 Portfolio Assessment 1 - Application.py
@@ -1,15 +1,31 @@
-a=int(input("Please enter the first number A"))
-b=int(input("Please enter the second number B"))
-
-def Euclidean(a,b):
-    while a*b!=0:
-        if a>b :
-            r=a%b
+def Euclidean(a,b):     #get GCD of two number
+    while a*b!=0:       #a!=0 and b!=0
+        if a>b :        #ensure a is always bigger
+            r=a%b       #mimic Euclidean
             a,b=b,r
         else:
-            a,b=b,a
+            a,b=b,a     #swap a,b
     return a
 
-a = Euclidean(a,b)
+
+#extend the Euclidean to check if two numbers are coprime
+
+def is_coprime(a,b):    #def the function
+    if Euclidean(a,b)==1:  #if gcd == 1 then yes
+        return 1
+    else:
+        return 0
+
+#main function
+
+a=int(input("Please enter the first number A"))   #input
+b=int(input("Please enter the second number B"))
+
+if is_coprime(a, b):                             #output
+    print("A and B are coprime")
+else:
+    print("A and B are not coprime")
+
+
         
-print(f"The GCD of A and B is {a}")
+
